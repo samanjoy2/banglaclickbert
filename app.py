108158ccbd13612d9484d75b6a695c7e96eac3d8
Saman Sarker Joy <samanjoy2@gmail.com>
2023-08-29 22:39:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ from transformers_interpret import SequenceClassificationExplainer
 
 @st.cache_data()
 def get_model():
-    model_name = "samanjoy2/banglabert_finetuned_sequence_classification_clickbait"
+    model_name = "samanjoy2/banglaclickbert_finetuned_sequence_classification_clickbait"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return tokenizer, model
 
 tokenizer,model = get_model()
 
+st.title('Bangla Clickbait Detection :sunglasses:')
+st.header('Check whether a Bangla News Headline is Clickbait or not', divider='rainbow')
 user_input = st.text_area('Enter Text to Analyze')
 button = st.button("Analyze")
 
@@ -20,7 +22,8 @@ if user_input and button :
     cls_explainer = SequenceClassificationExplainer(model, tokenizer)
     word_attributions = cls_explainer(user_input)
     st.write(cls_explainer.visualize())
+    st.divider()
     if cls_explainer.predicted_class_index == 1:
-        st.write('Lable: Clickbait')
+        st.subheader('Label Predicted: _Clickbait_')
     else:
-        st.write('Lable: Not-Clickbait')
+        st.subheader('Label Predicted: _Not-Clickbait_')
