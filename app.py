080aa2d3e65b9598092eb3edfc35a8b5657ad5f4
Saman Sarker Joy <samanjoy2@gmail.com>
2023-08-30 00:41:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def get_model():
 
 tokenizer,model = get_model()
 
+with st.sidebar:
+    st.subheader('Some Examples of Clickbait Headlines:')
+    st.write("অবশেষে মুখ খুললেন রাজ, যা বললেন পরীমনি ও সন্তান প্রসঙ্গে")
+    st.write("পান্তা ভাতের ইংরেজি কী জানেন? খুব সহজ! মাথায় আসছে না তো! জানুন")
+    st.write("এক টানেই আধ কোটির ইলিশ! সাগরে জাল ফেলতেই ভাগ্য বদল মৎস্যজীবীর! ইলিশে ইলিশে ছেয়ে গেল বাজার!")
+    st.subheader('Some Examples of Non-Clickbait Headlines:')
+    st.write("যুক্তরাজ্যে ফ্লাইট বিপর্যয়, ভোগান্তি থাকবে ‘কয়েকদিন’")
+    st.write("চট্টগ্রামে খাল–নালায় মানুষ মরছে, তবু নিরাপত্তাবেষ্টনী উঠছে না")
+    st.write("বায়ুদূষণে বাংলাদেশের মানুষের গড় আয়ু কমছে প্রায় ৭ বছর")
+    
+    
+
 st.title('Bangla Clickbait Detection :sunglasses:')
 st.header('Check whether a Bangla News Headline is Clickbait or not')
 user_input = st.text_area('Enter Text to Analyze')
