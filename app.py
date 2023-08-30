e011bcdba44c027fc9fb3395f59d2dfd15d47301
Saman Sarker Joy <samanjoy2@gmail.com>
2023-08-31 04:54:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
 from transformers_interpret import SequenceClassificationExplainer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 @st.cache(allow_output_mutation=True)
 def get_model():
@@ -11,30 +9,42 @@ def get_model():
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return tokenizer, model
 
-tokenizer,model = get_model()
-
-with st.sidebar:
-    st.subheader('Some Examples of Clickbait Headlines:')
-    st.write("অবশেষে মুখ খুললেন রাজ, যা বললেন পরীমনি ও সন্তান প্রসঙ্গে")
-    st.write("পান্তা ভাতের ইংরেজি কী জানেন? খুব সহজ! মাথায় আসছে না তো! জানুন")
-    st.write("এক টানেই আধ কোটির ইলিশ! সাগরে জাল ফেলতেই ভাগ্য বদল মৎস্যজীবীর! ইলিশে ইলিশে ছেয়ে গেল বাজার!")
-    st.subheader('Some Examples of Non-Clickbait Headlines:')
-    st.write("যুক্তরাজ্যে ফ্লাইট বিপর্যয়, ভোগান্তি থাকবে ‘কয়েকদিন’")
-    st.write("চট্টগ্রামে খাল–নালায় মানুষ মরছে, তবু নিরাপত্তাবেষ্টনী উঠছে না")
-    st.write("বায়ুদূষণে বাংলাদেশের মানুষের গড় আয়ু কমছে প্রায় ৭ বছর")
-    
-    
+tokenizer, model = get_model()
 
 st.title('Bangla Clickbait Detection :sunglasses:')
 st.header('Check whether a Bangla News Headline is Clickbait or not')
-user_input = st.text_area('Enter Text to Analyze')
-button = st.button("Analyze")
 
-if user_input and button :
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader('Some Examples of Clickbait Headlines:')
+    clickbait_examples = [
+        "অবশেষে মুখ খুললেন রাজ, যা বললেন পরীমনি ও সন্তান প্রসঙ্গে",
+        "পান্তা ভাতের ইংরেজি কী জানেন? খুব সহজ! মাথায় আসছে না তো! জানুন",
+        "এক টানেই আধ কোটির ইলিশ! সাগরে জাল ফেলতেই ভাগ্য বদল মৎস্যজীবীর! ইলিশে ইলিশে ছেয়ে গেল বাজার!"
+    ]
+    selected_clickbait = st.selectbox("Select Clickbait Example", clickbait_examples)
+    if st.button("Copy Clickbait Example"):
+        col2.text_area("Enter Text to Analyze", selected_clickbait)
+
+with col1:
+    st.subheader('Some Examples of Non-Clickbait Headlines:')
+    non_clickbait_examples = [
+        "যুক্তরাজ্যে ফ্লাইট বিপর্যয়, ভোগান্তি থাকবে ‘কয়েকদিন’",
+        "চট্টগ্রামে খাল–নালায় মানুষ মরছে, তবু নিরাপত্তাবেষ্টনী উঠছে না",
+        "বায়ুদূষণে বাংলাদেশের মানুষের গড় আয়ু কমছে প্রায় ৭ বছর"
+    ]
+    selected_non_clickbait = st.selectbox("Select Non-Clickbait Example", non_clickbait_examples)
+    if st.button("Copy Non-Clickbait Example"):
+        col2.text_area("Enter Text to Analyze", selected_non_clickbait)
+
+user_input = col2.text_area('Enter Text to Analyze')
+button = col2.button("Analyze")
+
+if user_input and button:
     cls_explainer = SequenceClassificationExplainer(model, tokenizer)
     word_attributions = cls_explainer(user_input)
     st.write(cls_explainer.visualize())
-    # st.divider()
     if cls_explainer.predicted_class_index == 1:
         st.subheader('Label Predicted: _Clickbait_')
     else:
